678268035317c967d48f465aaf6d4f3168b10bc9
root <root@finsweet.localdomain>
2017-08-24 02:30:19 +0000

    Additional environment variables

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
 
     mongodb:{
-        host:'mongodb://localhost/hellosign'
+        host: 'mongodb://'+process.env.MONGO_USER+':'+process.env.MONGO_PASS+'@'+process.env.MONGO_HOST || 'mongodb://localhost/hellosign'
     },
 
-    hellosignKey: '40b740615f6f87a49c72f6beefd4c880d476ee1c58994955a42bf33ad596e3b2',
-    hellosignID: '94cd46c0b0f52e25225f4406bdaa2b07'
+    hellosignKey: process.env.HELLO_KEY || '40b740615f6f87a49c72f6beefd4c880d476ee1c58994955a42bf33ad596e3b2',
+    hellosignID: process.env.HELLO_ID || '94cd46c0b0f52e25225f4406bdaa2b07'
 };
