e39cf545d92dab27a2a25659b92abcbaa7147aaf
root <root@finsweet.localdomain>
2017-08-24 02:09:56 +0000

    Initial Commit

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+    mongodb:{
+        host:'mongodb://localhost/hellosign'
+    },
+
+    hellosignKey: '40b740615f6f87a49c72f6beefd4c880d476ee1c58994955a42bf33ad596e3b2',
+    hellosignID: '94cd46c0b0f52e25225f4406bdaa2b07'
+};
